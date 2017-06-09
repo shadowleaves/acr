177b908ebd8b952f130df550bd0410071546ee54
Han Yan <hanyan1983@gmail.com>
2017-06-09 12:50:18 -0400

    multiprocesses

--- agg.py
+++ agg.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-import pandas as pd
-import subprocess
-from datetime import datetime
+# import pandas as pd
+# import subprocess
+# from datetime import datetime
 import os
 # import csv
-import shlex
-import json
-import bson
+# import shlex
+# import json
+# import bson
 import gzip
-from pytz import UTC
+# from pytz import UTC
 
 
 def ensure_path(*p):
@@ -22,31 +22,45 @@ def ensure_path(*p):
     return(f)
 
 
-def main():
-
-    from multiprocessing import Pool
-    from lib.bb.Symbol import Symbol
-    pool = Pool(processes=5)
-
-    exch = 'bbl1_equities_all'
-    path = os.path.expandvars('$HOME/data/bson/%s' % exch)
-    newpath = ensure_path('$HOME/Dropbox/intraday/bson/agg')
-
-    for sym in sorted(os.listdir(path)):
-        subpath = os.path.join(path, sym)
-        new_filepath = os.path.join(newpath, '%s.bson.gz' % sym)
-        print 'aggregating %s ...' % sym
+def aggregate(sym, source_path, dest_path):
+    subpath = os.path.join(source_path, sym)
+    new_filepath = os.path.join(dest_path, '%s.bson.gz' % sym)
+    print 'aggregating %s ...' % sym
+    try:
         with gzip.open(new_filepath, 'wb') as new_bson:
             for file in os.listdir(subpath):
                 filepath = os.path.join(subpath, file)
                 with gzip.open(filepath, 'rb') as bson_file:
                     for line in bson_file.readlines():
                         new_bson.write(line)
+    except KeyboardInterrupt:
+        print '^C detected...'
 
-                    # for doc in bson.decode_file_iter(bson_file):
-                    #     import pdb
-                    #     pdb.set_trace()
-        # raise SystemExit
+
+def main():
+
+    from multiprocessing import Pool
+    pool = Pool(processes=8)
+
+    # from multiprocessing.pool import ThreadPool
+    # pool = ThreadPool(processes=5)
+
+    exch = 'bbl1_equities_all'
+    path = os.path.expandvars('$HOME/data/bson/%s' % exch)
+    newpath = ensure_path('$HOME/Dropbox/intraday/bson/agg')
+    # newpath = ensure_path('/tmp/agg')
+
+    res = []
+    for sym in sorted(os.listdir(path)):
+        args = (sym, path, newpath)
+        # download_bars(*args)
+        result = pool.apply_async(aggregate, args=args)
+        res.append(result)
+
+    # retriving results (process won't start to run until here)
+    [x.get() for x in res]
+
+    # print 'total tasks %d' % len(tmp)
 
 
 if __name__ == '__main__':
--- bar.py
+++ bar.py
@@ -4,6 +4,7 @@ import subprocess
 from datetime import datetime
 import os
 # import csv
+import shlex
 import json
 import bson
 import gzip
@@ -21,8 +22,12 @@ def ensure_path(*p):
     return(f)
 
 
+def sym2uid(sym):
+    return sym.replace('-', '/').replace('/', '.')
+
+
 def run_iter(args):
-    args = args.split(' ')
+    args = shlex.split(args)
     p = subprocess.Popen(args, stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
     while(True):
@@ -33,74 +38,96 @@ def run_iter(args):
             break
 
 
-def download_bars(path, file, exch, sym, reset=False):
+def download_bars(path, file, exch, univ, reset=False):
     # tmp_file = '/tmp/bson.tmp'
 
     filepath = os.path.join(path, file)
 
-    date = file.split('.')[2]
-    bson_path = ensure_path('$HOME/Dropbox/intraday/bson/%s/%s' %
-                            (exch, sym))
-    bson_file = os.path.join(bson_path, '%s.bson.gz' % date)
+    date_str = file.split('.')[2]
+    picked = []
+    for sym in univ:
+        uid = sym2uid(sym)
+        bson_path = ensure_path('$HOME/data/bson/%s/%s' %
+                                (exch, uid))
+        bson_file = os.path.join(bson_path, '%s.bson.gz' % date_str)
+        if not os.path.exists(bson_file) or reset:
+            picked.append(sym)
 
-    if os.path.exists(bson_file) and not reset:
+    if len(picked) == 0:
         return
 
-    print 'producing %s ...' % bson_file
-    with gzip.open(bson_file, 'wb') as f_out:
+    print 'parsing %s for %d symbols ...' % (date_str, len(picked))
+    univ_str = ' '.join(['-S %s' % x for x in picked])
+    args = 'dfview --json --ignore-file-errors --threaded %s %s' % (
+        univ_str, filepath)
 
-        args = 'dfview --json --threaded --symbol %s %s' % (sym, filepath)
-        for line in run_iter(args):
-            if not len(line):
-                continue
+    files = {}
 
-            if 'error' in line or line[0] != '{':
-                break
+    for line in run_iter(args):
+        if not len(line):
+            continue
 
-            try:
-                dt = json.loads(line)
-            except ValueError as e:
-                print e
-                import pdb
-                pdb.set_trace()
+        if 'error' in line or line[0] != '{':
+            break
 
-            dt = {x: dt[x] for x in dt if dt[x] != 'NaN'}
-            for key in dt:
-                if 'time' in key:
-                    date = datetime.utcfromtimestamp(dt[key])
-                    date = date.replace(tzinfo=UTC)
-                    dt[key] = date
-            f_out.write(bson.BSON.encode(dt))
+        try:
+            dt = json.loads(line)
+        except ValueError as e:
+            print e
+            import pdb
+            pdb.set_trace()
+
+        dt = {x: dt[x] for x in dt if dt[x] != 'NaN'}
+        for key in dt:
+            if 'time' in key:
+                date = datetime.utcfromtimestamp(dt[key])
+                date = date.replace(tzinfo=UTC)
+                dt[key] = date
+
+        uid = sym2uid(dt['instr'])
+        if uid not in files:
+            bson_path = os.path.expandvars(
+                '$HOME/data/bson/%s/%s' % (exch, uid))
+            bson_file = os.path.join(bson_path, '%s.bson.gz' % date_str)
+            files[uid] = gzip.open(bson_file, 'wb')
+        files[uid].write(bson.BSON.encode(dt))
+        # f_out.write(bson.BSON.encode(dt))
+
+    for uid in files:
+        files[uid].close()
 
 
 def main():
 
     from multiprocessing import Pool
-    pool = Pool(processes=8)
+    from lib.bb.Symbol import Symbol
 
-    file = '~/Dropbox/intraday/DOW_univ.csv'
+    pool = Pool(processes=6)
+
+    file = '~/Dropbox/intraday/R1K_univ.csv'
     df = pd.read_csv(file, index_col=0)
     df = df[~df.sym.isnull()]
+    univ = [x for x in list(df.sym) if Symbol(x).is_valid()]
 
     root_path = '/nfs/data/hotel.inp/candlesticks/'
     # exch = 'shfe'
     exch = 'bbl1_equities_all'
-    for year in 2017, 2016:
-        for month in xrange(12):
+    for year in 2014, :
+        for month in xrange(13):
             path = os.path.join(root_path, exch, str(year), '%02d' % month)
             if not os.path.exists(path):
                 continue
+            res = []
             for file in sorted(os.listdir(path)):
                 if '.bb.gz' in file:
                     # filepath = os.path.join(path, file)
-                    res = []
-                    for sym in df.sym:
-                        args = (path, file, exch, sym)
-                        result = pool.apply_async(download_bars, args=args)
-                        res.append(result)
+                    args = (path, file, exch, univ)
+                    # download_bars(*args)
+                    result = pool.apply_async(download_bars, args=args)
+                    res.append(result)
 
-                    # retriving results (process won't start to run until here)
-                    tmp = [x.get(timeout=1e6) for x in res]
+            # retriving results (process won't start to run until here)
+            [x.get(timeout=1e6) for x in res]
 
         # raise SystemExit
 
